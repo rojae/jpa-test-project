b337403d9fa521096e7c3c1ac8b1adf9e6957d80
rojae <rojae@kakao.com>
2022-05-30 00:20:11 +0900

    Feat : Test2 N+1 문제 해결

--- src/main/java/kr/rojae/demo/user/UserRepository.java
+++ src/main/java/kr/rojae/demo/user/UserRepository.java
@@ -1,6 +1,7 @@
 package kr.rojae.demo.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,4 +9,10 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositorySupport {
     List<User> findAll();
+
+    @Query("select distinct u from USER u left join u.articles")
+    List<User> findAllJPQL();
+
+    @Query("select distinct u from USER u left join fetch u.articles")
+    List<User> findAllJPQLFetch();
 }
--- src/test/java/kr/rojae/demo/JPATest.java
+++ src/test/java/kr/rojae/demo/JPATest.java
@@ -27,4 +27,25 @@ public class JPATest {
         System.out.println("== end ==");
     }
 
+
+    // N+1 문제가 동일하게 발생한다.
+    // 아직까지는 지연로딩이 걸려있고, join을 했어도 프록시로 가져오는건 변함이 없기 때문이다.
+    @Test
+    @DisplayName("일반 jpql문, Join으로 N+1 문제는 풀리지 않는다.")
+    void testJPQL() {
+        System.out.println("== start ==");
+        List<User> users = userRepository.findAllJPQL();
+        System.out.println("== end ==");
+    }
+
+    // fetch는 지연 로딩이 걸려있는 연관관계에 대해서 한번에 같이 즉시로딩해주는 구문
+    @Test
+    @DisplayName("Fetch join을 통해서 N+1 문제가 해결이 된다.")
+    void testJPQLFetch() {
+        System.out.println("== start ==");
+        List<User> users = userRepository.findAllJPQLFetch();
+        System.out.println("== end ==");
+    }
+
+
 }
